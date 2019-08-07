def05be3e296bbcc506e1a499c2468e20b7163bb
Rishabh Gupta <rishabh.gupta@isteams-Mac-mini-23.local>
2019-08-08 01:57:34 +0530

    Basics paython exercise

---
+++ python.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print("hello")
+print(1+3)
+
+
+# In[9]:
+
+
+print("hello" +" world")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+greeting = "hello"
+name = input("please enter your name")
+print(greeting +" "+name)
+
+
+# In[1]:
+
+
+var1 = int(input("Enter value 1 = "))
+var2 = int(input("Enter value 2 = "))
+var3 = var1+var2
+print(var3)
+
+
+# In[17]:
+
+
+print('Print','kjkjkl',' Hello')
+
+
+# In[20]:
+
+
+print(" Rish\' \" s laptop")
+
+
+# In[25]:
+
+
+var1 = "Hello its rishabh"
+var2 = "doing python"
+message1 = f'{var1}, {var2}. In smartdata night'
+print(message1)
+print(message1[1:9])
+
+
+# In[26]:
+
+
+for i in range(1,13):
+    print(i)
+
+
+# In[27]:
+
+
+def add(var1,var2):
+    var3 = var1+var2
+    return var3
+
+
+# In[ ]:
+
+
+
+
